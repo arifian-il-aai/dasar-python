9dff7da6e184d98e22f58e1b19f4801fca293be9
arifian-il <yuotosaka43@gmail.com>
2024-02-25 23:36:48 +0700

    Commit pertamaku 2024

---
+++ dasar.py
@@ -0,0 +1,8 @@
+print("Halo Sobat Inners!")
+
+nama = "Arifian Saputra"
+umur = "22"
+pekerjaan = "Mahasiswa"
+
+print("Perkenalkan! aku " + nama + ", seorang "  + pekerjaan + " berumur " + umur + " tahun")
+print("Terima kasih!") 
